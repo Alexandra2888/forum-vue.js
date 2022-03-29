fb8c515683cea900e3c866e3b2fac300d3fa9e9c
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-03-29 20:42:07 +0300

    adding asyncDataStatus.js

---
+++ src/mixins/asyncDataStatus.js
@@ -0,0 +1,13 @@
+xport default {
+  data () {
+    return {
+      asyncDataStatus_ready: false
+    }
+  },
+  methods: {
+    asyncDataStatus_fetched () {
+      this.asyncDataStatus_ready = true
+      this.$emit('ready')
+    }
+  }
+}
